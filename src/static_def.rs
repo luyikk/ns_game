ed3e54a7a76402d7046d1e2e369f2daabc8c5cc6
luyi <luyikk@126.com>
2023-08-03 22:25:21 +0800

    feat: modify install

--- src/static_def.rs
+++ src/static_def.rs
@@ -1,46 +1,39 @@
 use aqueue::RwModel;
+use once_cell::sync::Lazy;
 use std::env::current_dir;
 use std::path::Path;
 
 use crate::config::Config;
 use crate::services::{BroadcastService, MasterService, ProxyService};
 
-lazy_static::lazy_static! {
-      /// 当前运行路径
-    pub static ref CURRENT_EXE_PATH:String={
-         match std::env::current_exe(){
-            Ok(path)=>{
-                if let Some(current_exe_path)= path.parent(){
-                    return current_exe_path.to_string_lossy().to_string()
-                }
-                panic!("current_exe_path get error: is none");
-            },
-            Err(err)=> panic!("current_exe_path get error:{err:?}")
+/// 当前运行路径
+pub static CURRENT_EXE_PATH: Lazy<String> = Lazy::new(|| match std::env::current_exe() {
+    Ok(path) => {
+        if let Some(current_exe_path) = path.parent() {
+            return current_exe_path.to_string_lossy().to_string();
         }
-    };
+        panic!("current_exe_path get error: is none");
+    }
+    Err(err) => panic!("current_exe_path get error:{err:?}"),
+});
 
-    /// 配置
-    pub static ref BASE_CONFIG:Config={
-        Config::load_config(&load_content("base_config.toml")
-            .expect("read base_config.toml file error"))
-            .expect("toml base_config.toml error")
-    };
+/// 配置
+pub static BASE_CONFIG: Lazy<Config> = Lazy::new(|| {
+    Config::load_config(
+        &load_content("base_config.toml").expect("read base_config.toml file error"),
+    )
+    .expect("toml base_config.toml error")
+});
 
-    /// 代理管理器
-    pub static ref PROXY:RwModel<ProxyService>={
-        RwModel::new(ProxyService::default())
-    };
+/// 代理管理器
+pub static PROXY: Lazy<RwModel<ProxyService>> = Lazy::new(|| RwModel::new(ProxyService::default()));
 
-    /// 广播服务
-    pub static ref BROADCAST_SERVICE:BroadcastService={
-        BroadcastService
-    };
+/// 广播服务
+pub static BROADCAST_SERVICE: Lazy<BroadcastService> = Lazy::new(|| BroadcastService);
 
-    /// MASTER 服务器
-    pub static ref MASTER_SERVICE:MasterService={
-        MasterService::new(BASE_CONFIG.master.clone())
-    };
-}
+/// MASTER 服务器
+pub static MASTER_SERVICE: Lazy<MasterService> =
+    Lazy::new(|| MasterService::new(BASE_CONFIG.master.clone()));
 
 /// 加载文件内容
 #[inline]
